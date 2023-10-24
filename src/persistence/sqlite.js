414290fd8901e7c4f4abd7cf633ac6484efca139
Norbert Krupa <101252684+Voirtez@users.noreply.github.com>
2023-10-24 11:40:58 +0100

    Update sqlite.js

--- src/persistence/sqlite.js
+++ src/persistence/sqlite.js
@@ -70,8 +70,8 @@ async function getItem(id) {
 async function storeItem(item) {
     return new Promise((acc, rej) => {
         db.run(
-            'INSERT INTO todo_items (id, name, completed) VALUES (?, ?, ?)',
-            [item.id, item.name, item.completed ? 1 : 0],
+            'INSERT INTO todo_items (id, name, priority, completed) VALUES (?, ?, ?, ?)', // Change here
+            [item.id, item.name, item.priority item.completed ? 1 : 0], // Change here
             err => {
                 if (err) return rej(err);
                 acc();
@@ -83,8 +83,8 @@ async function storeItem(item) {
 async function updateItem(id, item) {
     return new Promise((acc, rej) => {
         db.run(
-            'UPDATE todo_items SET name=?, completed=? WHERE id = ?',
-            [item.name, item.completed ? 1 : 0, id],
+            'UPDATE todo_items SET name=?, priority=?, completed=? WHERE id = ?', // Change here
+            [item.name, item.priority, item.completed ? 1 : 0, id], // Change here
             err => {
                 if (err) return rej(err);
                 acc();
