5b2941156e263c2244afc1878604f5e04eaa9551
Norbert Krupa <101252684+Voirtez@users.noreply.github.com>
2023-10-24 11:58:41 +0100

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -169,7 +169,7 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
                     </Button>
                 </Col>
                 <Col xs={9} className="name">
-                    {item.name} | Priority : {item.priority}
+                    {item.name} | Priority : {item.priority} // Change here
                 </Col>
                 <Col xs={1} className="text-center remove">
                     <Button
