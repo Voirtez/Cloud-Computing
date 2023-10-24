82a3b5c9c083f2e6e1b7749e05ad87b0b76016ff
Norbert Krupa <101252684+Voirtez@users.noreply.github.com>
2023-10-24 13:32:53 +0100

    Update sqlite.js

--- src/persistence/sqlite.js
+++ src/persistence/sqlite.js
@@ -18,7 +18,7 @@ function init() {
                 console.log(`Using sqlite database at ${location}`);
 
             db.run(
-                'CREATE TABLE IF NOT EXISTS todo_items (id varchar(36), name varchar(255), priority varchar(50), completed boolean)', // Change here
+                'CREATE TABLE IF NOT EXISTS todo_items (id varchar(36), name varchar(255), completed boolean)', // Change here
                 (err, result) => { 
                     if (err) return rej(err);
                     acc();
@@ -70,8 +70,8 @@ async function getItem(id) {
 async function storeItem(item) {
     return new Promise((acc, rej) => {
         db.run(
-            'INSERT INTO todo_items (id, name, priority, completed) VALUES (?, ?, ?, ?)', // Change here
-            [item.id, item.name, item.priority, item.completed ? 1 : 0], // Change here
+            'INSERT INTO todo_items (id, name, completed) VALUES (?, ?, ?)', // Change here
+            [item.id, item.name, item.completed ? 1 : 0], // Change here
             err => {
                 if (err) return rej(err);
                 acc();
@@ -83,8 +83,8 @@ async function storeItem(item) {
 async function updateItem(id, item) {
     return new Promise((acc, rej) => {
         db.run(
-            'UPDATE todo_items SET name=?, priority=?, completed=? WHERE id = ?', // Change here
-            [item.name, item.priority, item.completed ? 1 : 0, id], // Change here
+            'UPDATE todo_items SET name=?, completed=? WHERE id = ?', // Change here
+            [item.name, item.completed ? 1 : 0, id], // Change here
             err => {
                 if (err) return rej(err);
                 acc();
