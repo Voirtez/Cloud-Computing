3e3f7ecfa7703401fcaa8eb2e1ccf80f3072ece2
Norbert Krupa <101252684+Voirtez@users.noreply.github.com>
2023-10-24 15:15:19 +0100

    Update updateItem.js

--- src/routes/updateItem.js
+++ src/routes/updateItem.js
@@ -3,6 +3,7 @@ const db = require('../persistence');
 module.exports = async (req, res) => {
     await db.updateItem(req.params.id, {
         name: req.body.name,
+        priority: req.body.priority
         completed: req.body.completed,
     });
     const item = await db.getItem(req.params.id);
